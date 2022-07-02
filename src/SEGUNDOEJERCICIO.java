7820f1def1740d45a400a8de1b6523682f97a29f
santiagoruiz2003 <santiagoruiz5546@gmail.com>
2022-07-02 08:31:39 -0500

    segundo ejercicio de ciclos

---
+++ src/SEGUNDOEJERCICIO.java
@@ -0,0 +1,10 @@
+public class SEGUNDOEJERCICIO {
+    public static void main(String[] args) {
+        //escribir los numeros del 10 al 1
+        int numero = 10;
+        while (numero >= 1){
+            System.out.println("numero: "+numero);
+            numero--;
+        }
+    }
+}
