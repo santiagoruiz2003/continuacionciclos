6a30eb969484a29a9770fb6154b2bff4c1163dd5
santiagoruiz2003 <santiagoruiz5546@gmail.com>
2022-07-02 08:22:08 -0500

    se realiza ciclos while

--- src/continuacioncilos.java
+++ src/continuacioncilos.java
@@ -1,2 +1,16 @@
 public class continuacioncilos {
+    public static void main(String[] args) {
+        //ciclo while
+        //pintar los numeros del 1 al 10
+        boolean noHaTerminado = true;
+        int numero = 10;
+        while (noHaTerminado){
+        System.out.println("numero;" + numero);
+        numero = numero + 1;
+        if (numero >= 10){
+            noHaTerminado = false;
+        }
+
+        }
+    }
 }
